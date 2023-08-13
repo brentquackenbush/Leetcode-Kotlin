8702ef250a941792308e5316fb51d3cc67436425
Brent Quackenbush <brentprogramming@gmail.com>
2023-08-13 14:16:26 -0500

    Linked List implementation

---
+++ src/main/kotlin/kotlindatastructures/linkedlist/LinkedList.kt
@@ -0,0 +1,21 @@
+package kotlindatastructures.linkedlist
+
+class LinkedList<T : Any> {
+
+    private var head: Node<T>? = null
+
+    val isEmpty: Boolean
+        get() = head == null
+
+    fun append(value: T) {
+        if(head == null) {
+            head = Node(value)
+            return
+        }
+        var current = head
+        while(current?.next != null) {
+            current = current.next
+        }
+        current?.next = Node(value)
+    }
+}
