3a345a40e0f10f31a7d002b18209ce9b1a44d987
Brent Quackenbush <brentprogramming@gmail.com>
2023-09-03 18:42:39 -0500

    Notes for Hard Question

--- src/main/kotlin/dynamicprogramming/OddEvenJump.kt
+++ src/main/kotlin/dynamicprogramming/OddEvenJump.kt
@@ -46,5 +46,25 @@ class OddEvenJump {
 }
 // Solution
 /**
- * @Time_Complexity=
+ * @Time_Complexity=O(nlogn)
+ *
+ * Understanding the Problem
+ * The problem asks for the number of "good starting indices" in a given integer array. A starting index is considered "good" if you can reach the end of the array following a series of "jumps" that alternate between "odd-numbered" and "even-numbered" jumps.
+ *
+ * Odd-Numbered Jumps: You jump to an index j where the value is greater than or equal to the current value, and among those, the value should be the smallest possible.
+ *
+ * Even-Numbered Jumps: You jump to an index j where the value is less than or equal to the current value, and among those, the value should be the largest possible.
+ *
+ * Algorithmic Category
+ * This is a Dynamic Programming problem, particularly well-suited for bottom-up dynamic programming. The sequence and decisions to be made at each index make dynamic programming a natural fit for solving this problem.
+ *
+ * Algorithm to Solve
+ * Initialize Data Structures:
+ * Create two arrays (canReachEndOdd and canReachEndEven) of boolean values to store if it's possible to reach the end from index i with odd or even jumps respectively.
+ * A TreeMap to keep track of the possible jump destinations sorted by their values.
+ * Bottom-Up Dynamic Programming:
+ * Start from the end of the array and move to the beginning.
+ * At each step, use the TreeMap to find the next valid index for both odd and even jumps.
+ * Update canReachEndOdd[i] and canReachEndEven[i] based on the values for the next indices.
+ * Count Good Starting Indices: Count the number of indices from which you can reach the end of the array with odd-numbered jumps.
  */
