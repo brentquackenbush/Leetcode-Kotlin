4c134210baacf597fb5c8eef20d6cf8b9d6ef9ac
Brent Quackenbush <brentprogramming@gmail.com>
2023-09-30 15:48:03 -0500

    Easy

---
+++ src/main/kotlin/arraymanipulation/ValidParentheses.kt
@@ -0,0 +1,26 @@
+package arraymanipulation
+
+/**
+ * [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/description/)
+ *
+ * @Difficulty=Easy
+ */
+class ValidParentheses {
+
+    fun isValid(s: String): Boolean {
+
+        val stack = ArrayDeque<Char>()
+        for(ch in s) {
+            when(ch) {
+                '(', '{', '[' -> stack.addLast(ch)
+                ')' -> if(stack.isEmpty() || stack.removeFirst() != '(') return false
+                '}' -> if(stack.isEmpty() || stack.removeFirst() != '{') return false
+                ']' -> if(stack.isEmpty() || stack.removeFirst() != '[') return false
+            }
+        }
+        return stack.isEmpty()
+    }
+}
+/**
+ * @Time_Complexity=O(n)
+ */
