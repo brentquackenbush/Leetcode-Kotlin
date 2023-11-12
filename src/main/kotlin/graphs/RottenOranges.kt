3ee7fec764e9821ebbc35c3bfe24b4482ff83d4f
Brent Quackenbush <brentprogramming@gmail.com>
2023-11-12 11:34:47 -0600

    Medium graph bfs + dfs

--- src/main/kotlin/graphs/RottenOranges.kt
+++ src/main/kotlin/graphs/RottenOranges.kt
@@ -6,7 +6,7 @@ package graphs
  * @Difficulty=Medium
  */
 class RottenOranges {
-    fun orangesRotting(grid: Array<IntArray>): Int {
+    fun orangesRottingBfs(grid: Array<IntArray>): Int {
 
         val queue = ArrayDeque<Pair<Int,Int>>()
         var freshOranges = 0
@@ -46,6 +46,42 @@ class RottenOranges {
 
         return if(freshOranges == 0) time else -1 // Return the time if no fresh orange left, else -1
     }
+    fun orangesRottingDfs(grid: Array<IntArray>): Int {
+        if (grid.isEmpty() || grid[0].isEmpty()) return -1
+
+        // Start rotting process for each rotten orange found
+        for (i in grid.indices) {
+            for (j in grid[0].indices) {
+                if (grid[i][j] == 2) rotAdjacent(grid, i, j, 2)
+            }
+        }
+
+        // Find out the maximum time taken for any orange to rot, starting from 2
+        var minutes = 2
+        for (row in grid) {
+            for (cell in row) {
+                if (cell == 1) return -1 // If there's still a fresh orange, return -1
+                minutes = maxOf(minutes, cell) // Find the maximum time taken from all cells
+            }
+        }
+
+        return minutes - 2 // Subtract 2 to get the actual minutes count
+    }
+
+    private fun rotAdjacent(grid: Array<IntArray>, i: Int, j: Int, minutes: Int) {
+        if (i < 0 || i >= grid.size || j < 0 || j >= grid[0].size || grid[i][j] == 0 || (1 < grid[i][j] && grid[i][j] < minutes)) {
+            // If the cell is out of bounds, empty, or the orange is already rotten by another rotten orange, skip it
+            return
+        } else {
+            grid[i][j] = minutes // Mark the current orange as rotten with the current timestamp
+            // Recursively rot adjacent oranges with an incremented timestamp
+            rotAdjacent(grid, i - 1, j, minutes + 1)
+            rotAdjacent(grid, i + 1, j, minutes + 1)
+            rotAdjacent(grid, i, j - 1, minutes + 1)
+            rotAdjacent(grid, i, j + 1, minutes + 1)
+        }
+    }
+
 }
 // Solution
 /**
