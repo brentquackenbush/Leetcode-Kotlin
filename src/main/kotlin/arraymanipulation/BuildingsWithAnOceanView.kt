9685dd49ba0414acb65e964778b02807d3b72d1a
Brent Quackenbush <brentprogramming@gmail.com>
2023-11-19 16:00:47 -0600

    Medium. O(n)

---
+++ src/main/kotlin/arraymanipulation/BuildingsWithAnOceanView.kt
@@ -0,0 +1,30 @@
+package arraymanipulation
+
+/**
+ * [1762. Buildings With an Ocean View](https://leetcode.com/problems/buildings-with-an-ocean-view/description/)
+ *
+ * @Difficulty=
+ */
+class BuildingsWithAnOceanView {
+    fun findBuildings(heights: IntArray): IntArray {
+        val n = heights.size - 1
+        var currentTallestBuilding = heights[n]
+        val oceanViews = mutableListOf<Int>()
+        oceanViews.add(n)
+
+        if(n == 0) return oceanViews.toIntArray()
+
+        for(i in n - 1 downTo 0) {
+            if(heights[i] > currentTallestBuilding) {
+                oceanViews.add(i)
+                currentTallestBuilding = heights[i]
+            }
+        }
+
+        return oceanViews.reversed().toIntArray()
+    }
+}
+// Solution
+/**
+ * @Time_Complexity=O(n)
+ */
