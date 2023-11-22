c41ec88d62067d69ce8d1d60f05a914b910fe8e2
Brent Quackenbush <brentprogramming@gmail.com>
2023-11-21 20:10:52 -0600

    Easy

---
+++ src/main/kotlin/binarysearch/ClosetBinarySearchTreeValue.kt
@@ -0,0 +1,33 @@
+package binarysearch
+
+import bst.TreeNode
+import kotlin.math.abs
+
+/**
+ * [270. Closest Binary Search Tree Value](https://leetcode.com/problems/closest-binary-search-tree-value/description/)
+ *
+ * @Difficulty=Easy
+ */
+class ClosetBinarySearchTreeValue {
+    fun closestValue(root: TreeNode?, target: Double): Int {
+        var closetValue = root?.`val` ?: return -1
+        var current = root
+
+        while(current != null) {
+            if(abs(target - current.`val`) < abs(target - closetValue) || (abs(target - current.`val`) == abs(target - closetValue) && current.`val` < closetValue)) {
+                closetValue = current.`val`
+            }
+
+            current = if(current.`val` > target) {
+                current.left
+            } else {
+                current.right
+            }
+        }
+        return closetValue
+    }
+}
+// Solution
+/**
+ * @Time_Complexity=O(h)
+ */
